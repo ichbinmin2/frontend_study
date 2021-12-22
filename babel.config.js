849216ea26dc961387f16b7f17287611fc227cd7
JiyeonMin <ichbinmin2@gmail.com>
2021-12-22 18:25:04 +0900

    webpack.config.js 설치

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      // 미리 먼저 셋팅해놓은 기본 설정을 의미
       "@babel/preset-env", // 환경설정
-      "@babel/preset-react", // jsx를 바벨로 번역해준다는 뜻
+      "@babel/preset-react", // jsx를 리액트로 번역해라라는 뜻.
     ],
     plugins: [],
   };
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx", // 파일을 찾기 시작하는 입구에요.
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    // import from 뒤에 확장자를 생략했을 때, 어떤 확장자가 올 수 있는지 알려줘요.
+    // 알아서 파일 확장자를 찾아서 연결해주는 역할.
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader", // js와 jsx 파일을 가져오는 loader에요.
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"), // 번들이 만들어질 폴더 경로 -> 변경하고 build하면 됨.
+    filename: "bundle.js", // 만들어질 번들 파일의 이름 -> 변경하고 build하면 됨
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
