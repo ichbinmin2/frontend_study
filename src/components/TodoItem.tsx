f0fcfe5d1141d6e87fce25a6640b840903eb49d8
JiyeonMin <ichbinmin2@gmail.com>
2022-01-20 15:55:55 +0900

    changeTodoItem 으로 수정하기 기능 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 interface TodoItemProps {
   text: string;
@@ -6,7 +6,10 @@ interface TodoItemProps {
   id: string;
   deleteTodo: (targetId: string) => void;
   completeTodo: (targetId: string) => void;
+  changeTodoItem: (targetId: string, newText: string) => void;
 }
+
+// https://github.com/twinstae/realworld-react-redux/commit/b2b760f36198e19106676beb2ad4c624e0ca7f87#diff-e8d077acbe70741eb0e73c7919822d3931e52a385a15622eddd9e7123b7e81d9
 
 function TodoItem({
   text,
@@ -14,10 +17,37 @@ function TodoItem({
   id,
   deleteTodo,
   completeTodo,
+  changeTodoItem,
 }: TodoItemProps) {
+  const [editing, setEditing] = useState(false);
+  const [editingInput, setEditingInput] = useState(text);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setEditingInput(e.currentTarget.value);
+  }
+
+  function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      changeTodoItem(id, editingInput);
+      setEditing(false);
+    }
+  }
+
+  function handleDoubleClick() {
+    setEditing(true);
+  }
+
+  function handleBlur() {
+    setEditing(false);
+  }
+
   return (
-    <li className={completed ? "completed" : ""}>
-      <div className="view">
+    <li
+      className={
+        (completed ? "completed" : "") + " " + (editing ? "editing" : "")
+      }
+    >
+      <div className="view" onDoubleClick={handleDoubleClick}>
         <input
           className="toggle"
           type="checkbox"
@@ -27,7 +57,13 @@ function TodoItem({
         <label>{text}</label>
         <button className="destroy" onClick={() => deleteTodo(id)}></button>
       </div>
-      <input className="edit" value={text} />
+      <input
+        className="edit"
+        value={editingInput}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+        onBlur={handleBlur}
+      />
     </li>
   );
 }
